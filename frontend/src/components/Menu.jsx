f97855817b0a1395d723d8c3fe63320398df8398
viny <viny0698@gmail.com>
2023-12-10 00:55:56 +0530

    fix the client menu component link urls

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,27 +3,27 @@ import {Link} from "react-router-dom"
 function Menu () {
     return (
         <div className="flex flex-col p-3 w-72 shadow-lg h-full">
-            <Link to="/add_books">
+            <Link to="/librarymanagement/add_books">
                 <div className="p-3">
                     Add Books
                 </div>
             </Link>
-            <Link to="/borrow_history">
+            <Link to="/librarymanagement/borrow_history">
                 <div className="p-3">
                     Borrow History
                 </div>
             </Link>
-            <Link to="/issue_book">
+            <Link to="/librarymanagement/issue_book">
                 <div className="p-3">
                     Issue Book
                 </div>
             </Link>
-            <Link to="/search_books">
+            <Link to="/librarymanagement/search_books">
                 <div className="p-3">
                     Search Books
                 </div>
             </Link>
-            <Link to="/search_members">
+            <Link to="/librarymanagement/search_members">
                 <div className="p-3">
                     Search Members
                 </div>
