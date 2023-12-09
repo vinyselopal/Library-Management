827fb897427d9607d734baec5e137f34b8cee41a
viny <viny0698@gmail.com>
2023-12-10 00:37:40 +0530

    redirect root url to app url in django

--- backend/mylibrary/urls.py
+++ backend/mylibrary/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("librarymanagement/")),
     path("librarymanagement/", include("librarymanagement.urls")),
     path("admin/", admin.site.urls),
 ]
