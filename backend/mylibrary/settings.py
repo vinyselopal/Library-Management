5886095d52eb5602feb3c5a657f9bd956f30ad09
viny <viny0698@gmail.com>
2023-12-09 23:12:33 +0530

    create render.yaml and make production changes to settings

--- backend/mylibrary/settings.py
+++ backend/mylibrary/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,11 +36,17 @@ WEBPACK_LOADER = {
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%*q@^w5_aex!*sro_*&lc%p#!e#&-w&f-x+ae6n9i=w)$mwl-y"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    default="django-insecure-%*q@^w5_aex!*sro_*&lc%p#!e#&-w&f-x+ae6n9i=w)$mwl-y",
+)
 
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
